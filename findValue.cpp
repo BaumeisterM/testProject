a557a670d42dc5b384b72becc6624583e6ca6512
Michael Baumeister <baumeistermichael87@gmail.com>
2025-09-25 09:58:54 +0200

    Add sortArrays.cpp

---
+++ findValue.cpp
@@ -0,0 +1,33 @@
+//Aufgabe 3: Array-Suche mit Template
+
+//Schreibe eine Template-Funktion namens findValue, die ein Array und einen
+//Wert beliebigen Typs annimmt und den Index des ersten Vorkommens des Werts 
+//im Array zurückgibt. Sollte der Wert nicht gefunden werden, gib -1 zurück. 
+//Teste die Funktion mit einem Array aus std::string und einem Array aus int.
+
+//Funktions-Signatur:
+//template <typename T> int findValue(const T arr[], int size, const T& value)
+#include <iostream>
+#include <string>
+
+using namespace std;
+//Template um nach Elementen zu suchen
+template <typename T>
+int findValue(const T arr[], int size, const T& value){
+	for(int i=0;i<size;++i){
+		if(arr[i]==value){
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main(){
+	//String und Int Array deklariert und initialisiert
+	string abc[] = {"Hallo", "Welt", "und", "Hallo", "Geany", "!"};
+	int arr[] = {12,45,88,42,1337,102,-122,8,-42,-233,-1,1983};
+	//Suche nach dem Wort "Welt" im string-Array
+	cout << findValue(abc,sizeof(abc)/sizeof(abc[0]), string("Welt")) << " fuer string" << endl;
+	//Suche nach der Zahl "102" im int-Array
+	cout << findValue(arr,sizeof(arr)/sizeof(arr[0]), 102) << " fuer int" <<endl;
+}
---
+++ sortArrays.cpp
@@ -0,0 +1,40 @@
+//Aufgabe 2: Array-Sortierung mit Template
+
+//Erstelle eine Template-Funktion namens sortArray,
+//die ein Array beliebigen Typs und seine Größe als
+//Argumente entgegennimmt und das Array in aufsteigender 
+//Reihenfolge sortiert. Verwende den Bubble Sort oder
+//eine andere einfache Sortiermethode deiner Wahl. 
+//Teste die Funktion mit einem Array aus int und einem 
+//Array aus double.
+
+//Hinweis: Die Funktion soll void zurückgeben, 
+//da sie das übergebene Array direkt verändert.
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+void sortArray(T arr[], int size){
+	for(int i=0; i<size-1;++i){
+		for(int j=0;j<size-i-1;++j){
+			if(arr[j]>arr[j+1]){
+				swap(arr[j],arr[j+1]);
+			}
+		}
+	}
+	for(int i=0;i<size;++i){
+		cout << arr[i] << " ";
+	}
+	cout << endl; 
+}
+
+int main(){
+	int iArr[5]={4,7,1,9,2};
+	double dArr[7]={3.14, 13.37, 4.2, 11.11, 43.21, -73.31, -0.12};
+	char cArr[4]={'c','q','z','g'};
+	
+	sortArray(iArr,sizeof(iArr)/sizeof(iArr[0]));
+	sortArray(dArr,sizeof(dArr)/sizeof(dArr[0]));
+	sortArray(cArr,sizeof(cArr)/sizeof(cArr[0]));
+}
